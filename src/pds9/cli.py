7661c7a861f207cfee782c7f0f74eb98d228ee19
Joseph Hunkeler <jhunkeler@gmail.com>
2026-06-24 17:50:54 -0400

    Reuse interpreter where pds9 is installed
    * Change pds9-config argument -i (ini) to -p (print)

--- src/pds9/cli.py
+++ src/pds9/cli.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from string import Template
+import sys
 import pds9
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--ini", "-i", action="store_true", help="Return ds9 config snippet")
+    parser.add_argument("--print", "-p", action="store_true", help="Print ds9 config snippet")
 
     args = parser.parse_args()
     topdir = Path(pds9.__file__).parent
     inifile = topdir / "ds9.ini"
+    python_bin = Path(sys.prefix) / "bin" / "python3"
 
     if args.ini:
+        print(f"set pds9_python {python_bin}")
         print(f"source {inifile}")
